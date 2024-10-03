4a03c4c308970651aec361aa2c5cb832f6ec5218
Mateus Santos <mateuspele2015@gmail.com>
2024-10-03 15:40:06 -0300

    update: Acess history

--- src/config/Api.tsx
+++ src/config/Api.tsx
@@ -6,10 +6,10 @@ import Toast from 'react-native-toast-message';
 import { showToast } from '../utils/showToast';
 
 const api = axios.create({
-  baseURL: 'https://645b-179-186-19-111.ngrok-free.app',
+  baseURL: 'https://api.fonotherapp.com.br',
 });
 
-let isSessionExpiredToastShown = false; // Variável para controlar se o toast já foi mostrado
+let isSessionExpiredToastShown = false; 
 
 
 
--- src/screens/doctorServices/AccessHistory.tsx
+++ src/screens/doctorServices/AccessHistory.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { View, StyleSheet, Pressable } from 'react-native';
-import { Text, Button, List, Divider } from 'react-native-paper';
-import { colorPrimary } from '../../style/ColorPalette';
+import { Text, List, Divider } from 'react-native-paper';
 import { api } from '../../config/Api';
 import { Context } from '../../context/AuthProvider';
 import { FlatList } from 'react-native-gesture-handler';
@@ -9,71 +8,76 @@ import dayjs from 'dayjs';
 import LoadingComponent from '../../components/LoadingComponent';
 import { ContextPacient } from '../../context/PacientContext';
 
-const AccessHistoryFitice = [
-  { id: '1', data: '25/09/2024', hora: '14:32', local: 'São Paulo, Brasil' },
-  { id: '2', data: '24/09/2024', hora: '09:15', local: 'Rio de Janeiro, Brasil' },
-  { id: '3', data: '23/09/2024', hora: '19:45', local: 'Belo Horizonte, Brasil' },
-  { id: '4', data: '22/09/2024', hora: '07:20', local: 'Curitiba, Brasil' },
-];
-
 export default function AccessHistory({ navigation }) {
-  const { user } = useContext(Context)
+  const { user } = useContext(Context);
+  const { setPac_id } = useContext(ContextPacient);
   const [sessionsHistory, setSessionsHistory] = useState([]);
-  const [page, setPage] = useState<number>(1);
-  const { setPac_id, pac_id } = useContext(ContextPacient);
-
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false); const [hasMore, setHasMore] = useState(true);
 
   async function fetchSessions() {
+    if (isLoading || !hasMore) return;
+    setIsLoading(true);
     try {
-      const response = await api.get(`last-appointents/${user?.doctor?.doc_id}?page=${page}&pageSize=50`);
-      console.log(response.data);
-      setSessionsHistory(response.data.data)
+      const response = await api.get(`last-appointents/${user?.doctor?.doc_id}?page=${page}&pageSize=7`);
+      const newSessions = response.data.data;
+
+      if (newSessions.length === 0) {
+        setHasMore(false);
+      } else {
+        setSessionsHistory(prevSessions => [...prevSessions, ...newSessions]);
+      }
 
     } catch (error) {
       console.log(error);
-
+    } finally {
+      setIsLoading(false);
     }
   }
+
   useEffect(() => {
-    fetchSessions()
-  }, []);
-
-
+    fetchSessions();
+  }, [page]);
   const renderFooter = () => {
-    return (
-      <LoadingComponent />
-    )
-  }
-  function handleProfile(id: number) {
-    setPac_id(id)
-    navigation.navigate("PatientProfile");
-  }
-  const handleEndReached = () => {
-    setPage(prevPage => prevPage + 1);
+    if (!isLoading) return null;
+    return <LoadingComponent />;
   };
+
+  const handleProfile = (id: number) => {
+    setPac_id(id);
+    navigation.navigate('PatientProfile');
+  };
+
+  const handleEndReached = () => {
+    if (!isLoading && hasMore) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Histórico de Acesso {page}</Text>
+      <Text style={styles.title}>Histórico de Acesso</Text>
+
       <View style={styles.listContainer}>
         <FlatList
-          onEndReached={handleEndReached}
-          onEndReachedThreshold={0.1}
           data={sessionsHistory}
           keyExtractor={(item, index) => item?.ses_id.toString()}
+          onEndReached={handleEndReached}
+          onEndReachedThreshold={0.1}
           ListFooterComponent={renderFooter}
-
           renderItem={({ item }) => (
-            <Pressable onPress={() => handleProfile(item?.pacient?.pac_id)} key={item.id}>
+            <Pressable
+              style={styles.pressable}
+              onPress={() => handleProfile(item?.pacient?.pac_id)} key={item.id}>
               <List.Item
                 title={`${item.pacient.first_name}`}
-                description={`Sessão: ${dayjs(item.created_at).format("DD/MM/YYYY - HH:mm")} `}
+                description={`Sessão: ${dayjs(item.created_at).format('DD/MM/YYYY - HH:mm')}`}
                 left={(props) => <List.Icon {...props} icon="history" />}
               />
               <Divider />
             </Pressable>
           )}
         />
-
       </View>
     </View>
   );
@@ -90,13 +94,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
+  pressable: {
+    right: 15
+  },
   listContainer: {
     flexGrow: 1,
-    width: "100%",
-    borderWidth: 2
-  },
-  button: {
-    marginTop: 20,
-    padding: 10,
+    width: '100%',
+    borderWidth: 0,
   },
 });
